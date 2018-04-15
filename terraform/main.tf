2638a2301e47f692362f19d25997fcba97af0108
Dmitry Frolov <dmitry.for.work@yandex.ru>
2018-04-15 19:32:46 +0300

    Added main.tf config

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  version = "1.4.0"
+  project = "infra-198714"
+  region  = "europe-west1"
+}
+resource "google_compute_instance" "app" {
+    name         = "reddit-app"
+    machine_type = "g1-small"
+    zone         = "europe-west1-b"
+    tags = ["reddit-add"]
+    # определение загрузочного диска
+    boot_disk {
+        initialize_params {
+            image = "reddit-full"
+        }
+    }
+    # определение сетевого интерфейса
+    network_interface {
+        # сеть, к которой присоединить данный интерфейс
+        network = "default"
+        # использовать ephemeral IP для доступа из Интернет
+        access_config {}
+    }
+}
