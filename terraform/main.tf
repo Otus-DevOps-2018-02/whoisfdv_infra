f5411e915f3bda01145e649427580aed1c25b65d
Dmitry Frolov <dmitry.for.work@yandex.ru>
2018-04-19 13:29:20 +0300

    Расширен функционал main.tf, добавлены вспомогательные файлы для деплоя приложения

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@ provider "google" {
   project = "infra-198714"
   region  = "europe-west1"
 }
+
 resource "google_compute_instance" "app" {
     name         = "reddit-app"
     machine_type = "g1-small"
@@ -21,4 +22,35 @@ resource "google_compute_instance" "app" {
         # использовать ephemeral IP для доступа из Интернет
         access_config {}
     }
+    metadata {
+        ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
+    }
+    connection {
+        type     = "ssh"
+        user     = "appuser"
+        agent = false
+        private_key = "${file("~/.ssh/appuser")}"
+    }
+    provisioner "file" {
+        source      = "files/puma.service"
+        destination = "/tmp/puma.service"
+    }
+    provisioner "remote-exec" {
+        script = "files/deploy.sh"
+    }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+    name    = "allow-puma-default"
+    # Название сети, в которой действует правило
+    network = "default"
+    # Какой доступ разрешить
+    allow {
+        protocol = "tcp"
+        ports    = ["9292"]
+    }
+    # Каким адресам разрешаем доступ
+    source_ranges = ["0.0.0.0/0"]
+    # Правило применимо для инстансов с тегом ...
+    target_tags = ["reddit-app"]
 }
